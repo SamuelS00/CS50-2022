f46f50c303b3d42a20326b8e224e4929bad9d3f6
Samuel <samuelsoutosilva14@gmail.com>
2022-11-06 21:58:11 -0300

    feat: add mario - week1

---
+++ Week-1/mario-more/mario.c
@@ -0,0 +1,45 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int heigth;
+
+    // requests a valid number for the user
+    do
+    {
+        heigth = get_int("enter a number between 1 and 8: ");
+    }
+    while (heigth < 1 || heigth > 8);
+
+
+    for (int i = 0; i < heigth; i++)
+    {
+        // opposite opposite of index
+        int opposite = heigth - i - 1;
+        for (int j = 0; j < heigth; j++)
+        {
+            // compare opposite with row
+            if (opposite <= j)
+            {
+                printf("#");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        // add space between pyramids
+        printf("  ");
+
+        for (int j = 0; j < heigth; j++)
+        {
+            // compare opposite with row
+            if (j <= i)
+            {
+                printf("#");
+            }
+        }
+        printf("\n");
+    }
+}
