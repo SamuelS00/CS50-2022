091a334dc82579afb644a49fa26f5e822ff3900f
Samuel <samuelsoutosilva14@gmail.com>
2022-11-15 09:10:49 -0300

    refactor: add selectionSort and printArr funcitons

--- Week-3/examples/selection_sort.c
+++ Week-3/examples/selection_sort.c
@@ -1,31 +1,48 @@
 #include <cs50.h>
 #include <stdio.h>
-#include <string.h>
-
-// 
-int main(void)
+ 
+void selectionSort(int arr[], int n)
 {
-    int list_values[] = {7, 5, 1, 8, 3, 2};
-    int list_length = 5;
-
-    for (int i = 0; i <= list_length - 1; i++)
+    for (int i = 0; i <= n - 1; i++)
     {
         int min_index = i;
 
-        for (int j = i; j <= list_length - 1; j++)
+        for (int j = i; j <= n - 1; j++)
         {
-            if (list_values[j] < list_values[min_index])
+            if (arr[j] < arr[min_index])
             {
                 min_index = j;
             }
         }
 
-        if (list_values[i] > list_values[min_index])
+        if (arr[i] > arr[min_index])
         {
-            int aux = list_values[i];
-            list_values[i] = list_values[min_index];
-            list_values[min_index] = aux; 
+            int aux = arr[i];
+            arr[i] = arr[min_index];
+            arr[min_index] = aux; 
         }
-        printf("Number: %i\n", list_values[i]);
     }
+}
+
+void printArr(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf(" %d ", arr[i]);
+    }
+}
+
+int main(void)
+{
+    int list_values[] = {7, 5, 1, 8, 3, 2};
+    int n = sizeof(list_values) / sizeof(list_values[0]);
+
+    printf("unordered array: ");
+    printArr(list_values, n);
+
+    selectionSort(list_values, n);
+
+    printf("sorted arr: ");
+    printArr(list_values, n);
+    return 0;
 }
