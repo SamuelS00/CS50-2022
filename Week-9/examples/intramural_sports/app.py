058d77b544caee1980d3d28d56a207e7caaf6b24
Samuel <samuelsoutosilva14@gmail.com>
2022-12-17 11:08:51 -0300

    feat: add email submission upon completion of registration

--- Week-9/examples/intramural_sports/app.py
+++ Week-9/examples/intramural_sports/app.py
@@ -1,10 +1,24 @@
+import os
+
+
 from cs50 import SQL
 from flask import Flask, render_template, request, redirect
+from flask_mail import Mail, Message
+
 
 app = Flask(__name__)
 
-db = SQL("sqlite:///froshims.db")
+# Requires that "Less secure app access" be on
+# https://support.google.com/accounts/answer/6010255
+app.config["MAIL_DEFAULT_SENDER"] = os.environ.get("MAIL_DEFAULT_SENDER")
+app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASSWORD")
+app.config["MAIL_PORT"] = 587
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USERNAME"] = os.environ.get("MAIL_USERNAME")
+mail = Mail(app)
 
+db = SQL("sqlite:///froshims.db")
 
 SPORTS = ["Basketball", "Soccer", "Ultimate Frisbee"]
 
@@ -19,14 +33,19 @@ def register():
 
     # Validate submission
     name = request.form.get("name")
+    email = request.form.get("email")
     sport = request.form.get("sport")
-    if not name or sport not in SPORTS:
+    if not name or not email or sport not in SPORTS:
         return render_template("failure.html")
 
     # Remember registrant
     db.execute(
         "INSERT INTO registrants (name, sport) VALUES(?, ?)", name, sport
     )
+
+    # Send email
+    message = Message("You are registered!", recipients=[email])
+    mail.send(message)
 
     # Confirm registration
     return redirect("/registrants")
